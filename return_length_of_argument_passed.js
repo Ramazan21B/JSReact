37e23367a94994492af16ca6c3bfe52a4997a37e
Aitzhan Ramazan <ramazan@Aitzhans-MacBook-Pro.local>
2023-09-30 16:09:54 +0600

    commit

---
+++ return_length_of_argument_passed.js
@@ -0,0 +1,13 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+	let arr=[...args]
+    let len=arr.length;
+    return len
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
